8e4ff2ba16a7290cccc58c038ba14704caace664
Jasmineb2011 <Teddybear13.TB@gmail.com>
2019-04-25 10:45:55 +0300

    Fixed localhost

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const path = require('path');
 
 
-const port = 5001;
+const hostname = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 5001;
 const server = http.createServer(router);
 
 server.listen(port, () => {
-  console.log("Server is up runnig on port " + port);  
+  console.log(`Server running at port http://${hostname}:${port}`)
 });
